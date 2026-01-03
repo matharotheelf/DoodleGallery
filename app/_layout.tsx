73535b5695122dfd461655604bc6b3780c873f51
Tom Lawrence <mrthomasjacoblawrence@hotmail.co.uk>
2026-01-03 00:01:46 +0000

    Add splash screen on app load for awaiting loading fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { DefaultTheme, PaperProvider } from 'react-native-paper';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 const theme = {
   ...DefaultTheme,
@@ -13,6 +18,32 @@ const theme = {
 };
 
 export default function RootLayout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function doAsyncStuff() {
+      try {
+        // do something async here
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
+
+    doAsyncStuff();
+  }, []);
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hide();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
   return (
     <PaperProvider theme={theme}>
       <SafeAreaProvider>
